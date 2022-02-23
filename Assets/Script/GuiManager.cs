e4a08524676dc746f975ed4163a98d618b933450
olga794 <olga.franco@correounivalle.edu.co>
2022-02-23 01:32:25 -0500

    crear mapa personaje
    se integra una scenena para el nivel 1

---
+++ Assets/Script/GuiManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiManager : MonoBehaviour
+{
+    //public Image vistaNegra;
+    public float fadeSpeed;
+    public bool fadeToBlack, fadeFronBlack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
