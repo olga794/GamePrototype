7e0f7375fb2c8785ffd7dea5005923bd97a125f1
olga794 <olga.franco@correounivalle.edu.co>
2022-02-25 01:48:44 -0500

    se ajusta zon ade killer  y fondo para puntaje

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    private Vector3 respawnPosition;
+
+    private void Awake()
+    {
+        instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        respawnPosition = PlayerController.instance.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    public void Respawn()
+    {
+        StartCoroutine("RespawnWaiter");
+    }
+
+    public IEnumerator RespawnWaiter()
+    {
+        PlayerController.instance.gameObject.SetActive(false);
+        UIManager.Instance.fadeToBlack = true;
+        yield return new WaitForSeconds(2f);
+        UIManager.Instance.fadeFromBlack = true;
+        PlayerController.instance.transform.position = respawnPosition;
+        PlayerController.instance.gameObject.SetActive(true);
     }
 }
